016aa3ea5278ae7d4323a70aa79e2d4cdcb411a2
qiuxin <i@idx.best>
2023-08-20 21:17:50 +0800

    Add animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,39 @@
 import "./App.css";
 import { Box, Typography } from "@mui/material";
+import { motion } from "framer-motion";
 
 export function App() {
   return (
-    <Box sx={{ height: "100vh", overflow: "hidden" }}>
-      <Box
-        sx={{
+    <Box
+      sx={{
+        height: "100vh",
+        overflow: "hidden",
+        display: "flex",
+        background: "#ccc",
+      }}
+    >
+      <motion.div
+        initial={{
+          padding: "30px",
+          background: "#fff",
+          width: `${window.innerWidth / 2}px`,
           height: "100%",
-          overflow: "hidden",
+        }}
+        animate={{
+          width: "400px",
+          height: "100%",
           boxSizing: "border-box",
           padding: "30px",
           display: "flex",
           alignItems: "center",
+          background: "#fff",
         }}
+        transition={{ duration: 1 }}
       >
-        <Box sx={{ transform: "translateY(-50%)" }}>
+        <motion.div
+          animate={{ transform: "translateY(-50%)" }}
+          transition={{ duration: 1 }}
+        >
           <Typography variant="h1">
             秋信
             <Box component={"span"} sx={{ marginLeft: "8px" }}>
@@ -24,8 +43,8 @@ export function App() {
           <Typography sx={{ marginTop: "1rem" }}>
             秋天的信封，唤起了过往的思绪。
           </Typography>
-        </Box>
-      </Box>
+        </motion.div>
+      </motion.div>
     </Box>
   );
 }
